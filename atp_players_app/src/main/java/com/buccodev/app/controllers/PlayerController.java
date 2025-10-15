54ba94b21c0b4236201a3afcecc7190e96c73558
longobucco <longobuccodeb@gmail.com>
2025-10-14 23:56:42 -0300

    rename project directory

---
+++ atp_players_app/src/main/java/com/buccodev/app/controllers/PlayerController.java
@@ -0,0 +1,55 @@
+package com.buccodev.app.controllers;
+
+import com.buccodev.app.dtos.PlayerRequestDto;
+import com.buccodev.app.dtos.PlayerResponseDto;
+import com.buccodev.app.services.PlayerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static java.lang.IO.println;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PlayerResponseDto> createPlayer(@RequestBody PlayerRequestDto requestDto){
+        println(requestDto);
+        return ResponseEntity.ok(playerService.createPlayer(requestDto));
+    }
+
+    @PostMapping("/many")
+    public ResponseEntity<List<PlayerResponseDto>> createManyPlayers(@RequestBody List<PlayerRequestDto> requestDtos){
+        return ResponseEntity.ok(playerService.createManyPlayers(requestDtos));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlayerResponseDto> getPlayerById(@PathVariable UUID id){
+        return ResponseEntity.ok(playerService.getPlayerById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePlayer(@PathVariable UUID id){
+        playerService.deletePlayer(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlayerResponseDto> updatePlayer(@PathVariable UUID id, @RequestBody PlayerRequestDto requestDto){
+        return ResponseEntity.ok(playerService.updatePlayer(id, requestDto));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<PlayerResponseDto>> getPlayerByName(@RequestParam String name){
+        return ResponseEntity.ok(playerService.getPlayerByName(name));
+    }
+}
